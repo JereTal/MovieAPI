836ce7b0dec418f81784f282b5ee359fc6490a2a
JereTal <156084743+JereTal@users.noreply.github.com>
2024-12-22 18:19:36 +0200

    Added app.get files for databases and server fixes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { pgPool } from './pg_connection.js';
 
 var app = express();
 
@@ -13,6 +14,60 @@ app.get('/', (req, res) => {
     res.send('This is the root endpoint');
 })
 
-app.get('/user', (req, res) => {
+app.get('/movies', async (req, res) => {
+
+    try {
+        const result= await pgPool.query('SELECT * FROM movie');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(400).json({error: error.message});
+
+    }
+
     res.send('Getting user information');
 })
+
+app.get('/favorite_movies', async (req, res) => {
+    try {
+        const result = await pgPool.query('SELECT * FROM favorite_movie');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/movies', async (req, res) => {
+    try {
+        const result = await pgPool.query('SELECT * FROM movie');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/movie_api', async (req, res) => {
+    try {
+        const result = await pgPool.query('SELECT * FROM movie_api');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/movie_genres', async (req, res) => {
+    try {
+        const result = await pgPool.query('SELECT * FROM movie_genre');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
+app.get('/movie_reviews', async (req, res) => {
+    try {
+        const result = await pgPool.query('SELECT * FROM movie_review');
+        res.json(result.rows);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
